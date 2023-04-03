e20b7c947edda907f32811c32833fa1047079804
saratht48 <saratht199748@gmail.com>
2023-04-03 14:26:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import{BrowserRouter as Router,Routes,Route} from 'react-router-dom' 
+import Header from './components/Header'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Productlisting from './pages/Productlisting'
+import { Logincontextprovider } from './context/Logincontext'
+import Protectedroute from './hooks/Protectedroute'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Logincontextprovider>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={ <Login/>}></Route>
+        <Route path="/Dashboard" element={<Protectedroute><Dashboard/></Protectedroute>}></Route>
+        <Route path="/Productlisting" element={ <Productlisting/>}></Route>
+      </Routes>
+    </Router>
+    </Logincontextprovider>
   );
 }
 
